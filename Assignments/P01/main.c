8ad1785570743328c7ad9abb4e802b010400ce1e
Loic <konanloic10@gmail.com>
2021-09-30 23:07:38 -0500

    README.md

--- Assignments/P01/main.c
+++ Assignments/P01/main.c
@@ -1,13 +1,58 @@
-.model small
-.stack 100h
-.data
-message db 'hello world'
-.code
-mov ax,@data
-mov ds,ax
-mov ah,9
-mov dx,OFFSET message
-int 21h
-mov ah,4ch
-int 21h
-end
+//Mini Project 1
+//David Hawkins, Loic Konan, Sarah Gilliland
+
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<unistd.h>
+#include<stdio.h>
+#include<fcntl.h>
+#include<stdlib.h>
+
+int main()
+
+{
+	unsigned int pos = 0;
+	char character1;
+	char character2;
+	int instancesForThisChild = 0;
+	
+	char cmd[4];
+	
+	scanf("%4c", &cmd);
+	
+	while(cmd[0] != '/' && cmd[1] != '/' && cmd[2] != '/' && cmd[3] != '/')
+	{
+		pos = 0;
+		int file = open ("input.txt", O_RDONLY);
+		
+		for(int i = 0; i< 5 && pos < 20; i++)
+		{
+			instancesForThisChild = 0;
+			if(fork()==0){
+				for (int j = 0; j < 3; j++)
+				{
+					lseek(file, pos+j, SEEK_SET);
+					read(file, &character1, 1); //first character from file
+					lseek(file, pos+j+1, SEEK_SET);
+					read(file, &character2, 1); //read second character from file
+					
+					if ((character1 == cmd[2]) && (character2 == cmd[3]))
+					{
+						instancesForThisChild++;
+					}
+				}
+			printf("Child %d found %d instances.", i, instancesForThisChild);
+			printf("\n\n");
+			wait(NULL);
+			exit(0);
+			
+			}
+			pos = pos + 4;
+		}
+		
+		printf("\n\n");
+		scanf(" %4c", &cmd);
+	}
+	return 0;
+}
+
